b7d19c7b3438b84142d21fab596cab97d42987ce
lis <michuge@gmail.com>
2018-04-16 14:41:38 +0200

    Version 1.0

---
+++ sum_of_elements.py
@@ -0,0 +1,15 @@
+#Calculating sum of elements
+
+i = 20		#starting element
+j = 0
+
+while i<=136:			#final element
+
+	j=j+i 						#incrementing sum
+	print("\ncurrent element:",i)
+
+	i=i+1						#incrementing element/index
+	print("current sum:",j)
+
+
+print("\nfinal sum:", j)
